1b5855b7b68b79aa8cfbe3f78639452329c4d649
vladnega <negacevschi.vladimir@gmail.com>
2016-03-03 15:44:21 +0000

    remove domain.ts reference

--- srcjs/src/common/references/apiHelpers.ts
+++ srcjs/src/common/references/apiHelpers.ts
@@ -1,5 +1,4 @@
 /// <reference path="../../../typings/tsd.d.ts" />
-/// <reference path="domain.ts" />
 /// <reference path="../client/api.d.ts" />
 /// <reference path="../../../typings/ui-grid/ui-grid.d.ts" />
 
